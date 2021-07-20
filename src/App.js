d035a6b2b5161b1e936ff8dd886ee0c354bf8865
Prabhav Swaminathan <prabhavswaminathan@gmail.com>
2021-07-20 17:30:46 +1000

    inital commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Stop Wait a Minute!
         </p>
         <a
           className="App-link"
